c6ed342da48db433389d2b31bec4d672bf7e17c2
Roy Prins <prinsroy@gmail.com>
2019-08-09 16:12:54 +0200

    Workaround and lodash

--- src/index.js
+++ src/index.js
@@ -7,8 +7,16 @@ import schemaB from './schemaB'
 
 const log = (type) => console.log.bind(console, type);
 
+const formData = {
+  select1: "option1A",
+  option1A: {
+    select2: "option2A"
+  }
+}
+
 render((
   <div>
+    <h1>Form A</h1>
     <Form 
       key="a"
       schema={schemaA}
@@ -16,9 +24,18 @@ render((
       onSubmit={log("submitted")}
       onError={log("errors")}
     />
-    <div style={{margin: '100px'}}/>
+    <h2>Form B</h2>
     <Form 
       key="b"
+      schema={schemaB}
+      onChange={log("changed")}
+      onSubmit={log("submitted")}
+      onError={log("errors")} 
+    />
+    <h2>Form C</h2>
+    <Form 
+      key="c"
+      formData={formData}
       schema={schemaB}
       onChange={log("changed")}
       onSubmit={log("submitted")}
